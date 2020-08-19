304ee9ae3fa026bae9d289fbe18beb86e3250b80
Karsten Gresch <git-2015@by-doing.org>
2020-08-19 18:26:26 +0200

    domain model

---
+++ service-people/src/main/java/org/perceval/model/ProjectRole.java
@@ -0,0 +1,9 @@
+package org.perceval.model;
+
+import java.io.Serializable;
+
+public class ProjectRole implements Serializable {
+
+  private static final long serialVersionUID = -3879883974305470393L;
+
+}
